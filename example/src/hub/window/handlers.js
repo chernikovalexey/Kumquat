d1deea13b2865106e0403b4ebefe3615012430c8
Alexey Chernikov <chernikov.lesha@gmail.com>
2012-06-05 01:03:12 +0300

    Update

--- example/src/hub/window/handlers.js
+++ example/src/hub/window/handlers.js
@@ -6,7 +6,7 @@
   
   pl.extend(ke.app.handlers, {
     onItemClickHandle: function(e) {
-    	var content = pl(e.target).html();
+	    var content = pl(e.target).html();
       localStorage['SelectedItem'] = content;
       ke.app.render.organize.displayCurrentItem();
     }
